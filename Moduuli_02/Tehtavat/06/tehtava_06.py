fc971d9ec7d7d220f8a999fabb86d1ccf4b175d6
Aaro Saila <aaros.saila@gmail.com>
2023-08-22 16:47:05 +0300

    poistettu turha f-string

--- Moduuli_02/Tehtavat/06/tehtava_06.py
+++ Moduuli_02/Tehtavat/06/tehtava_06.py
@@ -2,7 +2,5 @@ import random
 numcode3 = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
 numcode4 = str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6))
 
-print(f"Kolminumeroinen lukukoodi, jonka luvut ovat väliltä 0 - 9:\n"
-      f"{numcode3}")
-print(f"\nNelinumeroinen lukukoodi, jonka luvut ovat väliltä 1 - 6:\n"
-      f"{numcode4}")
+print("Kolminumeroinen lukukoodi, jonka luvut ovat väliltä 0 - 9:\n" + numcode3)
+print("\nNelinumeroinen lukukoodi, jonka luvut ovat väliltä 1 - 6:\n" + numcode4)
