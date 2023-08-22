e8cf68659814c98db8e16761b31093d5fae0a2d8
Aaro Saila <aaros.saila@gmail.com>
2023-08-22 14:05:34 +0300

    hienosäätöä

--- Moduuli_02/Tehtavat/02/tehtava_02.py
+++ Moduuli_02/Tehtavat/02/tehtava_02.py
@@ -1,4 +1,6 @@
 import math
+
 r = float(input("Ympyrän säde: "))
 area = math.pi * (r ** 2)
+
 print(f"Ympyrän ala kahden desimaalin tarkkuudella: {area:.2f}")
--- Moduuli_02/Tehtavat/03/tehtava_03.py
+++ Moduuli_02/Tehtavat/03/tehtava_03.py
@@ -1,6 +1,8 @@
 a = float(input("Suorakulmion kanta: "))
 b = float(input("Suorakulmion korkeus: "))
+
 area = a * b
 perimeter = a * 2 + b * 2
+
 print(f"\nSuorakulmion ala: {area}")
 print(f"Suorakulmion piiri: {perimeter}")
