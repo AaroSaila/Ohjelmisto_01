b548f4c7c585acad70facada5df6c92463df8199
Aaro Saila <aaros.saila@gmail.com>
2023-09-04 15:24:19 +0300

    hienosäätöä

--- Moduuli_04/Tehtavat/06/tehtava_04_06.py
+++ Moduuli_04/Tehtavat/06/tehtava_04_06.py
@@ -1,12 +1,12 @@
-import random
+from random import uniform
 
 within = 0
 calcs = 0
 total = int(input("Kuinka monta pistettä lasketaan? "))
 
 while calcs < total:
-    x = random.uniform(-1, 1)
-    y = random.uniform(-1, 1)
+    x = uniform(-1, 1)
+    y = uniform(-1, 1)
     calcs += 1
 
     if (x ** 2) + (y ** 2) < 1:
