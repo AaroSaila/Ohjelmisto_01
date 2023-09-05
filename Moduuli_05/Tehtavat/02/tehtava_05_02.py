6cf2c3cdc9661846b3d76a4363329eff4158ac8a
Aaro Saila <aaros.saila@gmail.com>
2023-09-05 10:58:23 +0300

    hienosäätöä

--- Moduuli_05/Tehtavat/02/tehtava_05_02.py
+++ Moduuli_05/Tehtavat/02/tehtava_05_02.py
@@ -6,5 +6,9 @@ while num != "":
     nums.append(num)
     num = input("Anna kokonaisluku, tai lopeta ohjelma enterillä: ")
 
+for i in nums:
+    if nums.count(i) > 1:
+        nums.remove(i)
+
 nums.sort(reverse=True)
 print(f"Viisi suurinta antaamasi lukua olivat {nums[:5]}")
