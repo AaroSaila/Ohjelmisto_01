6021fc18916943af878c085db3cc60dcd2988012
Aaro Saila <aaros.saila@gmail.com>
2023-11-01 12:59:22 +0200

    tehtavia

---
+++ Moduuli_12/Tehtavat/tehtava12_1.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+def main():
+    try:
+        joke = requests.get("https://api.chucknorris.io/jokes/random")
+        if joke.status_code == 200:
+            json_joke = joke.json()
+            print(json_joke["value"])
+
+    except requests.exceptions.RequestException as e:
+        print("ERROR")
+
+
+main()
