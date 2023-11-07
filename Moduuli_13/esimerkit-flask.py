75e46b4e22cb70d1f32f0a36cf0a8df663ac4180
Aaro Saila <aaros.saila@gmail.com>
2023-11-07 14:44:13 +0200

    esimerkki

--- Moduuli_13/esimerkit-flask.py
+++ Moduuli_13/esimerkit-flask.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 
 app = Flask(__name__)
 
@@ -60,7 +60,8 @@ def page_not_found(virhekoodi):
         "teksti": "Virheellinen päätepiste"
     }
     jsonvast = json.dumps(vastaus)
-    return Response(response=jsonvast, status=404, mimetype="application/json")
+    # return Response(response=jsonvast, status=404, mimetype="application/json")
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
