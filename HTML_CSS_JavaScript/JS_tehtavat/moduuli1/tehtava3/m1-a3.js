09efe1d0d980c960134b79d38976ce289d18477d
Aaro Saila <aaros.saila@gmail.com>
2023-11-13 10:24:46 +0200

    asdasd

---
+++ HTML_CSS_JavaScript/JS_tehtavat/moduuli1/tehtava3/m1-a3.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const num1 = prompt('Input a number');
+const num2 = prompt('Input a number');
+const num3 = prompt('Input a number');
+
+const sum = num1 + num2 + num3;
+const product = num1 * num2 * num3;
+const average = sum / 3;
+
+document.querySelector('#sum').innerHTML = "Sum: " + num1 + " + " + num2 + " + " + num3 + " = " + sum;
+document.querySelector('#product').innerHTML = "Product: " + num1 + " * " + num2 + " * " + num3 + " = " + product;
+document.querySelector('#average').innerHTML = "Average: " + "(" + num1 + " + " + num2 + " + " + num3 + ")" + " / 3" + " = " + average;
