1b327e9a524b1195ecd0250fd358f5e405cf4ca8
Aaro Saila <aaros.saila@gmail.com>
2023-11-13 12:21:18 +0200

    asd

---
+++ HTML_CSS_JavaScript/JS_tehtavat/moduuli1/tehtava6/m1-a6.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const sqrt_element = document.querySelector('#square-root');
+const answer = confirm('Should I calculate the square root?');
+
+if (answer) {
+    const num = parseInt(prompt('Input a number: '));
+    if (num > 0) {
+        const sqrt = Math.sqrt(num);
+        sqrt_element.innerHTML = `The square root of ${num} is ${sqrt}`;
+    }
+    else {
+        sqrt_element.innerHTML = 'The square root of a negative number is not defined'
+    }
+}
