a535047b9d80bf0c188844515a8b962a53cf977b
Aaro Saila <aaros.saila@gmail.com>
2023-11-13 10:33:24 +0200

    asd

--- HTML_CSS_JavaScript/JS_tehtavat/moduuli1/tehtava3/m1-a3.js
+++ HTML_CSS_JavaScript/JS_tehtavat/moduuli1/tehtava3/m1-a3.js
@@ -8,6 +8,6 @@ const sum = num1 + num2 + num3;
 const product = num1 * num2 * num3;
 const average = sum / 3;
 
-document.querySelector('#sum').innerHTML = "Sum: " + num1 + " + " + num2 + " + " + num3 + " = " + sum;
-document.querySelector('#product').innerHTML = "Product: " + num1 + " * " + num2 + " * " + num3 + " = " + product;
-document.querySelector('#average').innerHTML = "Average: " + "(" + num1 + " + " + num2 + " + " + num3 + ")" + " / 3" + " = " + average;
+document.querySelector('#sum').innerHTML = `Sum: ${num1} + ${num2} + ${num3} = ${sum}`;
+document.querySelector('#product').innerHTML = `Product: ${num1} * ${num2} * ${num3} = ${product}`;
+document.querySelector('#average').innerHTML = `Average: (${num1} + ${num2} + ${num3}) / 3 = ${average}`;
