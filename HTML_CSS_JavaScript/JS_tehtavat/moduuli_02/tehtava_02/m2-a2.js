54795ddfb48463e136cf953497b79ff378a97f5e
Aaro Saila <aaros.saila@gmail.com>
2023-11-16 12:58:26 +0200

    rt

--- HTML_CSS_JavaScript/JS_tehtavat/moduuli_02/tehtava_02/m2-a2.js
+++ HTML_CSS_JavaScript/JS_tehtavat/moduuli_02/tehtava_02/m2-a2.js
@@ -1,3 +1,16 @@
 'use strict';
 
+const participants = [];
 const number_of_participants = parseInt(prompt('How many participants?'));
+
+for (let i = 1; i <= number_of_participants; i++) {
+    participants.push(prompt('Name of participant ' + i + ':'));
+}
+
+participants.sort();
+
+if (participants[0]) {
+    for (let i = 0; i <= participants.length; i++) {
+        document.querySelector('#list').innerHTML += `<li>${participants[i]}</li>`;
+    }
+}
