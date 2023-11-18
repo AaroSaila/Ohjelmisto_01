45bfa318d6c9578632cdba44fe43088ddf3b6428
Aaro Saila <aaros.saila@gmail.com>
2023-11-18 13:41:39 +0200

    asd

--- HTML_CSS_JavaScript/JS_tehtavat/moduuli_02/tehtava_06/m2-a6.js
+++ HTML_CSS_JavaScript/JS_tehtavat/moduuli_02/tehtava_06/m2-a6.js
@@ -5,20 +5,20 @@ let result;
 
 
 // Returns an integer between min and max (both included) [w3schools.com]
-/* function randomInt(min, max) {
-    return Math.floor(Math.random() + (max - min + 1)) + min;
-} */
-
-
-function dieRoll() {
-    return Math.floor(Math.random() + (6 - 1 + 1)) + 1;
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
-/* while (result != 6) {
+function dieRoll() {
+    return randomInt(1, 6);
+}
+
+
+while (result != 6) {
     result = dieRoll();
-    console.log(result);
+    /* console.log(result); */
     if (result != 0) {
         document.querySelector('#list').innerHTML += `<li>${result}</li>`;
     }
-} */
+}
