9faaa28b8b307838fb972b2597e6463e851a5337
Aaro Saila <aaros.saila@gmail.com>
2023-11-18 08:30:36 +0200

    asd

--- HTML_CSS_JavaScript/JS_tehtavat/moduuli_02/tehtava_03/m2-a3.js
+++ HTML_CSS_JavaScript/JS_tehtavat/moduuli_02/tehtava_03/m2-a3.js
@@ -1,5 +1,14 @@
 'use strict';
 
-for (let i = 1; i <= 5; i++) {
-    
+const dogs = [];
+
+for (let i = 1; i <= 6; i++) {
+    dogs.push(prompt(`Dog (${i})`));
+}
+
+dogs.sort();
+dogs.reverse();
+
+for (let i = 0; i < dogs.length; i++) {
+    document.querySelector('#list').innerHTML += `<li>${dogs[i]}</li>`;
 }
