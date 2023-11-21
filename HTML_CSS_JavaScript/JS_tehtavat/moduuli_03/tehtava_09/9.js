57282e66ec059b8780b5b67f55e5a5ae4b759c5b
Aaro Säilä <aaros.saila@gmail.com>
2023-11-21 13:23:11 +0200

    asd

---
+++ HTML_CSS_JavaScript/JS_tehtavat/moduuli_03/tehtava_09/9.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+const calculation = document.getElementById('calculation');
+const button = document.getElementById('start');
+const result = document.getElementById('result');
+
+function calculation(calc_array) {
+    /* if (calc_array.includes()) */
+}
+
---
+++ Kertaus/kertaus09-13.py
@@ -0,0 +1,10 @@
+class Eläin:
+    def __init__(self, ):
+
+
+    def tee_ääni():
+        return
+
+
+eläin1 = Eläin("leijona", "karjuu")
+eläin1.tee_ääni()
