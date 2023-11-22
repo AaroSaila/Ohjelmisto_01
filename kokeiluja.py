1e515f7b471f8a1b9790bc46576a64f9c0482ab9
Aaro Säilä <aaros.saila@gmail.com>
2023-11-22 10:45:28 +0200

    asd

--- kokeiluja.py
+++
@@ -1,6 +0,0 @@
-API_key = "404f93a3ca1a1e108fa623b09646cb76"
-zip_code = "01390"
-country_code = "FI"
-request = (f"https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country_code}&appid={API_key}"
-           f"&lang=fi&units=metric")
-print(request)
